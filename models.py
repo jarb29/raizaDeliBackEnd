b04a5789cd3f3debca484459b877da4fc468e4d1
jarb29 <jarb29@gmail.com>
2020-06-21 23:13:46 -0400

    base de datos

--- models.py
+++ models.py
@@ -9,7 +9,8 @@ from flask import Flask
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'MySQL: jarb29$default'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 
